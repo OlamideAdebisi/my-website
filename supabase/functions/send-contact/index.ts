e547d68f09abdc866b0b1659a68a56ca36bac945
OlamideAdebisi <olamide.adebisi18@gmail.com>
2026-09-24 15:40:56 +0100

    Allow contact form CORS preflight

--- supabase/functions/send-contact/index.ts
+++ supabase/functions/send-contact/index.ts
@@ -1,6 +1,6 @@
 import { serve } from 'https://deno.land/std@0.224.0/http/server.ts'
 
-const cors = { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type' }
+const cors = { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type', 'Access-Control-Allow-Methods': 'POST, OPTIONS' }
 
 serve(async (req) => {
   if (req.method === 'OPTIONS') return new Response('ok', { headers: cors })
